78efed890f59ac1eded12340f77e5ba4bd591ffd
smallaitt <536762164@qq.com>
2015-07-05 23:07:55 +0800

    为了修改path为PATH，因为path经常会被作为局部变量使用。

--- hh/core/base/__module.ts
+++ hh/core/base/__module.ts
@@ -1,7 +1,7 @@
 /**
  * Created by SmallAiTT on 2015/6/29.
  */
-///<reference path="path1.ts" />
+///<reference path="PATH.ts" />
 ///<reference path="STR.ts" />
 ///<reference path="ColorMatrix.ts" />
 ///<reference path="Matrix.ts" />
